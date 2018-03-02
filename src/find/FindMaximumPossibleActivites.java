de85852ddb2288db4b5d4e65047567f3fbb8bd1d
tituslesley89 <lesley.pandian@thomsonreuters.com>
2018-03-02 14:51:32 -0600

    Fixed typo in FindMaximumPossibleActivities.java

--- src/find/FindMaximumPossibleActivites.java
+++ src/find/FindMaximumPossibleActivites.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class FindMaxiumPossibleActivites {
+public class FindMaximumPossibleActivites {
 
     private static int findMaxiumPossibilities(List<Activity> activities) {
         if(activities.size() == 1) {
